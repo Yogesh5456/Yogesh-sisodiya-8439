7bf133edc77fef4c76f49de223b57ffa0ff263f7
Yogesh5456 <yogeshsisodiya897879@gmail.com>
2024-02-20 13:02:19 +0530

    updated

---
+++ Q2.php
@@ -0,0 +1,12 @@
+<!-- Traffics light Simulation -->
+<?php
+$color="Red";
+if($color=="Green"){
+    echo "Green - Go";
+}elseif($color=="Yellow"){
+    echo "Yellow - Slow down";
+}elseif($color="Red"){
+    echo "Red - Stop";
+}else{
+    echo "Invalid color";
+}
